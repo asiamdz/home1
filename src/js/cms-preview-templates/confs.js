6c2f4490586a8d9735b28faa38989a96f10a50ba
asiamdz <asiam.soc.dz@gmail.com>
2024-12-07 11:34:08 +0100

    Update and rename values.js to confs.js

--- src/js/cms-preview-templates/confs.js
+++ src/js/cms-preview-templates/confs.js
@@ -17,19 +17,19 @@ const MediaBlock = ({heading, text, imageUrl, reverse}) => {
   </div>;
 };
 
-export default class ValuesPreview extends React.Component {
+export default class ConfsPreview extends React.Component {
   render() {
     const {entry, getAsset} = this.props;
 
     const image = getAsset(entry.getIn(["data", "image"]));
-    const entryValues = entry.getIn(["data", "values"]);
-    const values = entryValues ? entryValues.toJS() : [];
+    const entryConfs = entry.getIn(["data", "confs"]);
+    const confs = entryConfs ? entryconfs.toJS() : [];
 
     return <div>
       <Jumbotron image={image} title={entry.getIn(["data", "title"])} />
       <div className="bg-off-white pv4">
         <div className="mw7 center ph3 pt4">
-          {values.map(({text, heading, imageUrl}, i) =>
+          {confs.map(({text, heading, imageUrl}, i) =>
             <MediaBlock key={i} text={text} heading={heading} imageUrl={imageUrl} reverse={i % 2 === 0} />
           )}
         </div>
